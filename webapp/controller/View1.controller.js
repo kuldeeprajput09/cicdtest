94a5b8c19f61a99c8807a690d7e7065bfd04d2bc
Kuldeep Singh <z004jn6u>
2024-01-12 10:06:42 +0000

    cicd changes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,19 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,MessageToast) {
         "use strict";
 
         return Controller.extend("cicdtest.controller.View1", {
             onInit: function () {
 
+            },
+            clickbutton: function (evt) {
+                MessageToast.show(evt.getSource().getId() + " Pressed");
             }
         });
     });
