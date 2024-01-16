98a6c0e1b55e0662ba1e4cae534b0c15bf028344
kuldeeprajput09 <102403396+kuldeeprajput09@users.noreply.github.com>
2024-01-16 17:06:00 +0530

    Update View1.controller.js

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -14,8 +14,5 @@ sap.ui.define([
             },
             clickbutton: function (evt) {
                 MessageToast.show(evt.getSource().getId() + " Pressed");
-            },clickbutton: function (evt) {
-                MessageToast.show(evt.getSource().getId() + " Pressed");
             }
-        });
     });
